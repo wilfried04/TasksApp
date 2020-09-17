c264f2a0bd2e65248def18a6df904a2123cd10e6
wilromeo <wilfriedkp.romeo@mail.com>
2020-09-17 21:38:26 +0100

    function CRUD

--- db/migrate/20200917173252_create_tasks.rb
+++ db/migrate/20200917173252_create_tasks.rb
@@ -1,11 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.string :detail
-      t.date :start_time
-      t.date :stop_time
-      t.string :priority
+      t.string :name, precence:true, null:false ,uniqueness: true, length:{ maximum:60 }
+      t.text :detail,precence:true,null:false
 
       t.timestamps
     end
