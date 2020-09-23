653194e7f71dbbc2a5a019c8349bbe820d3d0ea8
wilromeo <wilfriedkp.romeo@mail.com>
2020-09-23 15:40:36 +0100

    time zone Japon

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module TasksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'UTC'
+    config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
 
