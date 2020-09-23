a00b89e571c18497b3b80cbdc283b6f2876ad410
wilromeo <wilfriedkp.romeo@mail.com>
2020-09-23 13:37:01 +0100

    Step 3 Basic Settings + Deployment requirements checklist

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+#config.time_zone = 'Tokyo'
+#config.active_record.default_timezone = :local
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,13 +11,9 @@ module TasksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'UTC'
     config.active_record.default_timezone = :local
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
---
+++ spec/system/task_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+#bundle exec rspec spec/system/task_spec.rb
+
+RSpec.describe 'Task management function', type: :system do
+  describe 'New creation function' do
+    context 'When creating a new task' do
+      it 'The created task is displayed' do
+        #creer un enregistrement avec factory
+        task = FactoryBot.create(:task, name:'task', detail:'content')
+        #après la création rediriger vers l'index
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
+    context 'When tasks are arranged in descending order of creation date and time' do
+      it 'New task is displayed at the top' do
+        task = FactoryBot.create(:task, name:'title1', detail:'content1')
+        task = FactoryBot.create(:task, name:'title2', detail:'content2')
+        visit tasks_path
+        task_list = all('.task_list')
+        expect(task_list[0]).to have_content 'title2'
+        expect(task_list[1]).to have_content 'title1'
+        #expect(page).to have_content 'task'
+      end
+   end
+  end
+  describe 'List display function' do
+    context 'When transitioning to the list screen' do
+      it 'The created task list is displayed' do
+        task = FactoryBot.create(:task, name:'task', detail:'content')
+        #après la création rediriger vers l'index
+        visit tasks_path
+        #binding.irb
+        current_path
+        Task.count
+        page.html
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+  describe 'Detailed display function' do
+     context 'When transitioned to any task details screen' do
+       it 'The content of the relevant task is displayed' do
+         visit tasks_path
+         click_on 'task'
+         expect(page).to have_content 'task'
+       end
+     end
+  end
+end
