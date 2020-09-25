8ea8b42e4e34eeb50d4b98a9531882a9eb70c055
wilromeo <wilfriedkp.romeo@mail.com>
2020-09-25 13:54:26 +0100

    step3

--- app/models/task.rb
+++
@@ -1,9 +0,0 @@
-class Task < ApplicationRecord
-    validates :name, presence:true, null:false,length:{maximum:60}
-    validates :datail, presence:true, null:false
-    validates :end_time, presence:true, null:false
-    enum statut: %i[not_started started completed]
-    enum priority: %i[low medium high]
-    scope :name_search,  -> (text_serach) {where("name LIKE ?", "%#{text_serach}%")}
-    scope :status_search,  -> (text_serach) {where(status: text_serach)}  
-end
