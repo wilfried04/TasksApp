6c517393dfb7551a3718629421616324a59ef2cd
wilromeo <wilfriedkp.romeo@mail.com>
2020-09-28 09:32:24 +0100

    test spec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,25 +1,16 @@
-# 「FactoryBotを使用します」という記述
 FactoryBot.define do
-
-  # 作成するテストデータの名前を「task」とします
-  # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :task do
-    name { 'factory_name_1' }
-    content { 'factory_詳細1' }
-    priority { '低' }
+    name { 'Task1' }
+    detail { 'content1' }
+    deadline { '2020-05-01' }
+    priority { 'high' }
+    statut { 'completed' }
   end
-
-  # 作成するテストデータの名前を「second_task」とします
-  # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
   factory :second_task, class: Task do
-    name {'factory_name_2'}
-    content { 'factory_詳細2'}
-    priority { '中' }
+    name { 'Task2' }
+    detail { 'content2' }
+    deadline { '2020-05-02' }
+    priority { 'medium' }
+    statut { 'completed' }
   end
-
-  factory :third_task, class: Task do
-    name {'factory_name_3'}
-    content { 'factory_詳細3'}
-    priority { '高' }
-  end
-end
+ end
