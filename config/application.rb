833d38236c852aa81efdebd76b90301b7988ae0d
wilromeo <wilfriedkp.romeo@mail.com>
2020-09-29 06:44:03 +0100

    step3

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module TasksApp
                 routing_specs: false,
                 controller_specs: false,
                 request_specs: false
-      #g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
