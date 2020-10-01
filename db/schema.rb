59074859cb142dab5161ebd6f12f68d47605da90
wilromeo <wilfriedkp.romeo@mail.com>
2020-10-01 21:18:57 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2020_09_17_173252) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.string "detail"
+    t.string "name", null: false
+    t.text "detail", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
