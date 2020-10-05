2e7e226be3c54d729a42832ff72475adb940e647
wilromeo <wilfriedkp.romeo@mail.com>
2020-10-05 13:21:20 +0100

    model user and migration users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_104929) do
+ActiveRecord::Schema.define(version: 2020_10_05_121738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,19 @@ ActiveRecord::Schema.define(version: 2020_09_15_104929) do
     t.datetime "updated_at", null: false
     t.integer "statut"
     t.integer "priority"
+    t.bigint "user_id"
     t.index ["name", "statut"], name: "index_tasks_on_name_and_statut"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "tasks", "users"
 end
